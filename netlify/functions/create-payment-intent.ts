25a557833964cea4d91ecc8d6d5f16e857f198cd
Ilshae <pruszynskaingrid@outlook.com>
2024-03-16 15:23:39 +0100

    debugging stripe

--- netlify/functions/create-payment-intent.ts
+++ netlify/functions/create-payment-intent.ts
@@ -1,5 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 require("dotenv").config();
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
